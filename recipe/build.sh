72877ce2131e8dc2cb5dbac019f3fdc30ebb4b64
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-04-01 13:11:39 -0700

    Tune recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+perl -pi -e "s#CMAKE_CXX_STANDARD\s+\d+#CMAKE_CXX_STANDARD 17#g" CMakeLists.txt 
+#patch < ~/projects/xalan-c-feedstock/patches/dead_code_patch.patch
+
 mkdir build && cd build
 cmake -D CMAKE_BUILD_TYPE=Release\
   -D CMAKE_INSTALL_PREFIX=$PREFIX\
   -D CMAKE_PREFIX_PATH=$PREFIX \
+  -D CMAKE_POLICY_VERSION_MINIMUM=3.5\
+  -D CMAKE_C_FLAGS="-Wno-error" \
+  -D CMAKE_CXX_FLAGS="-Wno-error -fpermissive -std=gnu++17"  \
+  -DCMAKE_VERBOSE_MAKEFILE=ON\
+  -DCMAKE_CXX_STANDARD=17\
   $SRC_DIR
 make -j $CPU_COUNT install
+
